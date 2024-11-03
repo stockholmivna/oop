6f231651b58a80149ee8de1a736b5b9ee95c671d
stockholmivna <annizhin@gmail.com>
2024-11-03 17:31:29 +0200

    commit 2

--- lab_3/Java/src/main/java/com/gildedrose/GildedRose.java
+++ lab_3/Java/src/main/java/com/gildedrose/GildedRose.java
@@ -5,16 +5,44 @@ class GildedRose {
 
     public GildedRose(Item[] items) {
         this.items = items;
-    }
+            }
+        }
 
-    public void updateQuality() {
+    public void updateQuality() { 
         for (int i = 0; i < items.length; i++) {
             updateItemQuality(items[i]); //i call the new method to exchange the nested logic
+            updateSellIn(items[i]); //new method to update sellIn
+            handleExpiredItems(items[i]); //analogous to the previous method
+    }
+
+    private void increaseQuality(Item item) { 
+        if (item.quality < 50) {
+            item.quality += 1;
         }
     }
 
+    //New method selIn to avoid nested logic
+    private void updateSellIn(Item item) { //we use the same logic as before, selIn decrease by 1 for every item except Sulfuras
+        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+        item.sellIn -= 1;
+    }
+}
+
+    private void handleExpiredItems(Item item) { //new method to handle expired items
+        if (item.sellIn >= 0) return; //if sellIn is greater than 0, we return
+
+        if (item.name.equals("Aged Brie")) { //condition for Aged Brie
+            increaseQuality(item);
+        } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+             item.quality = 0; //if the item is a Backstage pass the quality is set to 0
+        } else {
+                    decreaseQuality(item); //i will add a new method soon
+                }
+            }
+    }
+
 //i add new method to avoid nested logic
-    private void updateItemQuality(Item item) {
+private void updateItemQuality(Item item) {
         if (!item.name.equals("Aged Brie") 
         && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
             if (item.quality > 0 && !item.name.equals("Sulfuras, Hand of Ragnaros")) {
