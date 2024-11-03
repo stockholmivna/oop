189d4d5717e514c45d474d3ce8ff0018320bd205
stockholmivna <annizhin@gmail.com>
2024-11-03 22:23:05 +0200

    8

--- lab_3/Java/src/main/java/com/gildedrose/Item.java
+++ lab_3/Java/src/main/java/com/gildedrose/Item.java
@@ -3,9 +3,9 @@ package com.gildedrose;
 public class Item {
 
     public String name;
-
+    
     public int sellIn;
-
+    
     public int quality;
 
     public Item(String name, int sellIn, int quality) {
@@ -14,8 +14,42 @@ public class Item {
         this.quality = quality;
     }
 
-   @Override
-   public String toString() {
+    public void increaseQuality() {
+        if (quality < 50) {
+            quality++;
+        }
+    }
+
+    public void decreaseQuality() {
+        if (quality > 0 && !isSulfuras()) {
+            quality--;
+        }
+    }
+
+    public void decreaseSellIn() {
+        if (!isSulfuras()) {
+            sellIn--;
+        }
+    }
+
+    public boolean isExpired() {
+        return sellIn < 0;
+    }
+
+    public boolean isAgedBrie() {
+        return name.equals("Aged Brie");
+    }
+
+    public boolean isBackstagePass() {
+        return name.equals("Backstage passes to a TAFKAL80ETC concert");
+    }
+
+    public boolean isSulfuras() {
+        return name.equals("Sulfuras, Hand of Ragnaros");
+    }
+
+    @Override
+    public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
     }
 }
